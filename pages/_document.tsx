d396c7af1fb0e4fd5c4130651f1fa678a26b8946
Andres Aguilera <andres.aguilera@startgrid.com>
2023-01-23 16:11:35 -0300

    prettier

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title="Andrés Aguilera - Front End Challeng" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
